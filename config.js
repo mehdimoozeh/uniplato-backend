7f80138c8e1f2c0beb368c0b3ce87234e5e8048f
Mehdi <mohammadmoozeh@gmail.com>
2021-03-27 18:50:54 +0430

    add mysql configs

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 'use strict'
 
 const environment = {
-  port: process.env.PORT || 8000
+  port: process.env.PORT || 8000,
+  mysql: {
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3306,
+    username: process.env.MYSQL_USERNAME || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || 'uniplato'
+  }
 }
 
 exports.env = environment
